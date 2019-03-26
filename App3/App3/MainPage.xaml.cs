af184de45ffa5acdba3bbcd42a0f949bd0adc6bc
henrydg09 <henrydg09@gmail.com>
2019-03-26 09:42:08 -0300

    a

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -9,9 +9,26 @@ namespace App3
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
-		{
-			InitializeComponent();
-		}
-	}
-}
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, Event args)
+        {
+            Label1.IsVisible = false;
+            Label1.IsVisible = false;
+            if (Entry2.text != null &&
+                Entry2.Text.Lenght > 0 &&
+                Picker.SelectedIndex >= 0)
+            {
+                int periodo = int.Parse(Entry2.text);
+                int semestre = Picker.SelectedIndex + 1;
+                if periodo == semestre
+            {
+                    Label1.IsVisible = true;
+                } else
+                {
+                    Label2.IsVisible = true;
+                }
+             
