562f8ccdf20b5bf80cb299975b6bb5b7f3d9cdf8
henrydg09 <henrydg09@gmail.com>
2019-03-26 09:49:20 -0300

    versão estruturada

--- App3/App3/MainPage.xaml.cs
+++ App3/App3/MainPage.xaml.cs
@@ -7,30 +7,27 @@ using Xamarin.Forms;
 
 namespace App3
 {
-    public partial class MainPage : ContentPage
-    {
+	public partial class MainPage : ContentPage
+	{
         public MainPage()
         {
             InitializeComponent();
-        }
+            {
 
-        void OnButtonClicked(object sender, Event args)
-        {
-            Label1.IsVisible = false;
-            Label1.IsVisible = false;
-            if (Entry2.text != null &&
-                Entry2.Text.Lenght > 0 &&
-                Picker.SelectedIndex >= 0)
-            {
-                int periodo = int.Parse(Entry2.text);
-                int semestre = Picker.SelectedIndex + 1;
-                if periodo == semestre
-            {
-                    Label1.IsVisible = true;
-                } else
+                void OnButtonClicked(object sender, Event args)
                 {
-                    Label2.IsVisible = true;
-                }
-            }
-        }
-    }
+                    Label1.IsVisible = false;
+                    Label1.IsVisible = false;
+                    if (Entry2.text != null &&
+                        Entry2.Text.Lenght > 0 &&
+                        Picker.SelectedIndex >= 0)
+                    {
+                        int periodo = int.Parse(Entry2.text);
+                        int semestre = Picker.SelectedIndex + 1;
+                        if periodo == semestre
+                    {
+                            Label1.IsVisible = true;
+                        } else
+                        {
+                            Label2.IsVisible = true;
+                        }
